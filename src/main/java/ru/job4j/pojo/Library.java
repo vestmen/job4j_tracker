577d1b12ac60585befd74bfdc766c709aedc7c33
AlexNemtsev <alex.nemtsev95@gmail.com>
2022-12-18 13:28:46 +0300

    3. Массивы и модели. [#174892 #372202]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -23,7 +23,7 @@ public class Library {
         }
         System.out.println("Clean code");
         for (Book book : books) {
-            if (book.getName().equals("Clean code")) {
+            if ("Clean code".equals(book.getName())) {
                 System.out.println(book.getName() + " - " + book.getCount() + " pages");
             }
         }
