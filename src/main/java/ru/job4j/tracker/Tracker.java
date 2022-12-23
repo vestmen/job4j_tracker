f90aa0777949997adbe587e32a989ec43b9d3ccb
AlexNemtsev <alex.nemtsev95@gmail.com>
2022-12-23 18:01:30 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #372409]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -58,13 +58,14 @@ public class Tracker {
     }
 
     public boolean delete(int id) {
+        boolean rsl = false;
         int index = indexOf(id);
         if (index != -1) {
             System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
-            return true;
+            rsl = true;
         }
-        return false;
+        return rsl;    
     }
 }
