ac9bd7ce0c5649993f4487d2ba8133d02e82d8a0
AlexNemtsev <alex.nemtsev95@gmail.com>
2023-02-03 19:26:57 +0300

    4. Телефонный справочник на базе ArrayList [#41598 #387374]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -23,7 +23,6 @@ public class PhoneDictionaryTest {
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
         ArrayList<Person> persons = phones.find("alex");
-        ArrayList<Person> expected = new ArrayList<>();
-        assertThat(persons).isEqualTo(expected);
+        assertThat(persons.isEmpty()).isTrue();
     }
 }
