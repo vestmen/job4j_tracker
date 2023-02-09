23ab6a997a0864853d7d93de44b58340d7ddc113
AlexNemtsev <alex.nemtsev95@gmail.com>
2023-02-09 18:16:47 +0300

    0. Сортировка [#10096]

--- src/test/java/ru/job4j/tracker/ItemTest.java
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -3,7 +3,6 @@ package ru.job4j.tracker;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -12,7 +11,7 @@ public class ItemTest {
     @Test
     public void whenItemAscByName() {
         List<Item> items = new ArrayList<>(List.of(new Item("A"), new Item("D"), new Item("C"), new Item("B")));
-        Collections.sort(items, new ItemAscByName());
+        items.sort(new ItemAscByName());
         List<Item> expected = new ArrayList<>(List.of(new Item("A"), new Item("B"), new Item("C"), new Item("D")));
         assertThat(items).isEqualTo(expected);
     }
@@ -20,7 +19,7 @@ public class ItemTest {
     @Test
     public void whenItemDescByName() {
         List<Item> items = new ArrayList<>(List.of(new Item("A"), new Item("D"), new Item("C"), new Item("B")));
-        Collections.sort(items, new ItemDescByName());
+        items.sort(new ItemDescByName());
         List<Item> expected = new ArrayList<>(List.of(new Item("D"), new Item("C"), new Item("B"), new Item("A")));
         assertThat(items).isEqualTo(expected);
     }
